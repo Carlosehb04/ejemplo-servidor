f27974499314fe067165e33b13e24518ba60d553
Carlos <carlosehernandezb26@gmail.com>
2026-06-17 12:50:38 +0200

    agregando cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use( cors({
+    origin:"http://localhost:5173",
+}))
 
 app.get('/', (req, res) => {
     res.json({
