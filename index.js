5fb7ab5a60b10861138c7f3d869ff9994d9b6b7f
Carlos <carlosehernandezb26@gmail.com>
2026-06-17 11:32:50 +0200

    primer commint

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
